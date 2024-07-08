12f9902ebaf394eaab2adb0b2176507d324f426f
Ralina Salikhova <ralina.salikhova48@gmail.com>
2024-07-08 10:27:51 +0300

    update

--- src/application.js
+++ src/application.js
@@ -31,99 +31,97 @@ export default () => {
       document.querySelector('.lead').textContent = i18nInstance.t('interface.p');
       document.querySelector('label').textContent = i18nInstance.t('interface.label');
       document.querySelector('.col-auto > button').textContent = i18nInstance.t('interface.button');
-    });
 
-  const watchedState = view(i18nInstance, state);
-
-  yup.setLocale({
-    mixed: {
-      notOneOf: i18nInstance.t('messages.existsURL'),
-    },
-    string: {
-      url: i18nInstance.t('messages.invalidURL'),
-    },
-  });
-
-  const getURL = (link) => `https://allorigins.hexlet.app/get?disableCache=true&url=${link}`;
-
-  const getData = (link) => axios
-    .get(getURL(link))
-    .then((response) => parseData(response.data.contents));
-
-  const addFeedsAndPosts = (data, currState) => {
-    const feedId = uniqueId();
-    const { feedTitle, feedDescription, feedPosts } = data;
-    const postsWithId = feedPosts.map((post) => ({ ...post, id: uniqueId() }));
-    currState.feeds.push({
-      title: feedTitle,
-      description: feedDescription,
-      id: feedId,
-    });
-    currState.posts.push(...postsWithId);
-  };
-
-  const catchDataErrors = (err) => {
-    if (err instanceof AxiosError) {
-      watchedState.form.errors = i18nInstance.t('messages.networkError');
-    } else if (err.message === 'parsingError') {
-      watchedState.form.errors = i18nInstance.t('messages.parsingError');
-    }
-  };
-
-  const updatePosts = (interval = 5000) => {
-    const update = () => {
-      const links = state.loadedURL;
-      if (links.length !== 0) {
-        const titles = state.posts.map(({ title }) => title);
-        const updatedPosts = links.map((link) => getData(link).then((data) => data.feedPosts));
-        const promise = Promise.all(updatedPosts);
-        promise
-          .then((data) => data.flat().filter((post) => !titles.includes(post.title)))
-          .then((newPosts) => {
-            watchedState.posts.push(...newPosts);
-          })
-          .then(() => setTimeout(update, interval))
-          .catch((err) => catchDataErrors(err));
-      }
-    };
-    setTimeout(update, interval);
-  };
-
-  const getSchema = (URLs) => {
-    const schema = yup.string().required().url().notOneOf(URLs);
-    return schema;
-  };
-
-  const { postsContainer } = interfaceElements;
-  postsContainer.addEventListener('click', (e) => {
-    const currId = e.target.dataset.id;
-    const { posts } = state;
-    const currPost = posts.find((post) => post.id === currId);
-    watchedState.currentPost = currPost;
-    state.readPostsId.push(currPost.id);
-  });
-
-  const { form } = interfaceElements;
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const url = formData.get('url');
-    getSchema(watchedState.loadedURL)
-      .validate(url)
-      .then((validUrl) => {
-        getData(validUrl)
-          .then((loadedData) => {
-            addFeedsAndPosts(loadedData, watchedState);
-            watchedState.loadedURL.push(validUrl);
-          })
-          .catch((err) => catchDataErrors(err));
-      })
-      .catch((err) => {
-        if (err instanceof yup.ValidationError) {
-          [watchedState.form.errors] = err.errors;
-        }
+      const watchedState = view(i18nInstance, state);
+      yup.setLocale({
+        mixed: {
+          notOneOf: i18nInstance.t('messages.existsURL'),
+        },
+        string: {
+          url: i18nInstance.t('messages.invalidURL'),
+        },
       });
-  });
+      const getURL = (link) => `https://allorigins.hexlet.app/get?disableCache=true&url=${link}`;
 
-  updatePosts();
+      const getData = (link) => axios
+        .get(getURL(link))
+        .then((response) => parseData(response.data.contents));
+
+      const addFeedsAndPosts = (data, currState) => {
+        const feedId = uniqueId();
+        const { feedTitle, feedDescription, feedPosts } = data;
+        const postsWithId = feedPosts.map((post) => ({ ...post, id: uniqueId() }));
+        currState.feeds.push({
+          title: feedTitle,
+          description: feedDescription,
+          id: feedId,
+        });
+        currState.posts.push(...postsWithId);
+      };
+
+      const catchDataErrors = (err) => {
+        if (err instanceof AxiosError) {
+          watchedState.form.errors = i18nInstance.t('messages.networkError');
+        } else if (err.message === 'parsingError') {
+          watchedState.form.errors = i18nInstance.t('messages.parsingError');
+        }
+      };
+
+      const updatePosts = (interval = 5000) => {
+        const update = () => {
+          const links = state.loadedURL;
+          if (links.length !== 0) {
+            const titles = state.posts.map(({ title }) => title);
+            const updatedPosts = links.map((link) => getData(link).then((data) => data.feedPosts));
+            const promise = Promise.all(updatedPosts);
+            promise
+              .then((data) => data.flat().filter((post) => !titles.includes(post.title)))
+              .then((newPosts) => {
+                watchedState.posts.push(...newPosts);
+              })
+              .then(() => setTimeout(update, interval))
+              .catch((err) => catchDataErrors(err));
+          }
+        };
+        setTimeout(update, interval);
+      };
+
+      const getSchema = (URLs) => {
+        const schema = yup.string().required().url().notOneOf(URLs);
+        return schema;
+      };
+
+      const { postsContainer } = interfaceElements;
+      postsContainer.addEventListener('click', (e) => {
+        const currId = e.target.dataset.id;
+        const { posts } = state;
+        const currPost = posts.find((post) => post.id === currId);
+        watchedState.currentPost = currPost;
+        state.readPostsId.push(currPost.id);
+      });
+
+      const { form } = interfaceElements;
+      form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const url = formData.get('url');
+        getSchema(watchedState.loadedURL)
+          .validate(url)
+          .then((validUrl) => {
+            getData(validUrl)
+              .then((loadedData) => {
+                addFeedsAndPosts(loadedData, watchedState);
+                watchedState.loadedURL.push(validUrl);
+              })
+              .catch((err) => catchDataErrors(err));
+          })
+          .catch((err) => {
+            if (err instanceof yup.ValidationError) {
+              [watchedState.form.errors] = err.errors;
+            }
+          });
+      });
+
+      updatePosts();
+    });
 };
